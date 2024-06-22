9a32d863043340b82431ab780bfc5cf65e9289f7
Yaduvanshirahul <ry421072@gmail.com>
2024-06-22 14:57:00 +0530

    22-06-2024

---
+++ Class2.java
@@ -0,0 +1,51 @@
+//import java.util.Arrays;
+
+public class Class2 {
+
+    static class Student {
+
+        /*
+         * If Student is a non-static inner class, you must first create an instance of
+         * the outer class Class2 before you can create an instance of Student
+         */
+
+        int rno;
+        String name;
+        float marks = 34;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         * 
+         * Class2 outerInstance = new Class2();
+         * --> if inner class is not static then create the instance of outer class
+         * 
+         * Student[] students = new Student[5];
+         * 
+         * System.out.println(Arrays.toString(students));
+         * 
+         * --> when no value has initialize its value is null for array of students
+         * 
+         */
+
+        Student[] students = new Student[5];
+
+        Student rahul = new Student();
+        /*
+         * System.out.println(rahul.rno);
+         * System.out.println(rahul.marks);
+         * System.out.println(rahul.name);
+         * 
+         * --> gives default value for all the variable as no value has assign
+         */
+
+        rahul.rno = 23;
+        rahul.name = "shyam";
+        // rahul.marks = 45.5f;
+        System.out.println(rahul.rno);
+        System.out.println(rahul.name);
+        System.out.println(rahul.marks);
+    }
+
+}
---
+++ Class3.java
@@ -0,0 +1,40 @@
+public class Class3 {
+
+    static class Student {
+        int rno;
+        String name;
+        float marks;
+
+        Student(int rno, String name, float marks) {
+            this.rno = rno;
+            this.name = name;
+            this.marks = marks;
+        }
+
+        Student() {
+            this.rno = 14;
+            this.name = "Shyam";
+            this.marks = 34.4f;
+        }
+    }
+
+    public static void main(String[] args) {
+        Student[] students = new Student[5];
+
+        /*
+         * Student kunal = new Student(32, "Rahul", 45);
+         * 
+         * System.out.println(kunal.rno);
+         * System.out.println(kunal.name);
+         * System.out.println(kunal.marks);
+         * 
+         */
+
+        Student rahul = new Student();
+
+        System.out.println(rahul.rno);
+        System.out.println(rahul.name);
+        System.out.println(rahul.marks);
+
+    }
+}
